a4bc181724015a24aea602a21f6fc682be1788ca
Philip Norton <philipnorton42@gmail.com>
2013-11-09 21:46:31 +0000

    Added playbooks and the docroot directory.

---
+++ docroot/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head> 
+<meta charset="UTF-8">
+<title>Test</title>
+</head>
+<body>
+  <header>
+    <h1>Test</h1>
+  </header>  <section>
+    <article>
+      <p><?php echo 'This works!'; ?></p>
+    </article>
+  </section>
+</body>
+</html>
