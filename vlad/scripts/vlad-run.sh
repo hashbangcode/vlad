c95ed97b503ec42402c68fda8819b0723efdc624
Philip Norton <philipnorton42@gmail.com>
2015-04-13 15:35:11 +0100

    Added some comments to the new vlad script

--- vlad/scripts/vlad-run.sh
+++ vlad/scripts/vlad-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# Find the current directory of the script being run.
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   TARGET="$(readlink "$SOURCE")"
@@ -15,13 +15,16 @@ done
 RDIR="$( dirname "$SOURCE" )"
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+# Change to the script directory.
 cd $DIR
 
+# Now move two directories abobe, which puts us into the Vagrant directory.
 cd ../../
 
+# Grab any tags that have been sent to the script.
 ANSIBLE_TAGS=$1
 
-if [ -z "$ANSIBLE_TAGS" ]; then
+if [ -z "$ANSIBLE_TAGS" ]; thenß
     echo "Running Vlad playbook"
     ansible-playbook --inventory-file=./.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=~/.vagrant.d/insecure_private_key -u vagrant ./vlad/playbooks/site.yml
 fi
