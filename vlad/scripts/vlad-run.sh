51bf3605bf71426b4ca12495874d7589a66cd480
Philip Norton <philipnorton42@gmail.com>
2015-04-13 09:30:03 +0100

    added a script to allow the unusable playbook to be run without lots of flags on the command line (issue #202)

---
+++ vlad/scripts/vlad-run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  TARGET="$(readlink "$SOURCE")"
+  if [[ $SOURCE == /* ]]; then
+    SOURCE="$TARGET"
+  else
+    DIR="$( dirname "$SOURCE" )"
+    SOURCE="$DIR/$TARGET"
+  fi
+done
+
+RDIR="$( dirname "$SOURCE" )"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
+
+cd ../../
+
+ANSIBLE_TAGS=$1
+
+if [ -z "$ANSIBLE_TAGS" ]; then
+    echo "Running Vlad playbook"
+    ansible-playbook --inventory-file=./.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=~/.vagrant.d/insecure_private_key -u vagrant ./vlad/playbooks/site.yml
+fi
+
+if [ ! -z "$ANSIBLE_TAGS" ]; then
+    echo "Running Vlad playbook with the tags '$ANSIBLE_TAGS'"
+    ansible-playbook --inventory-file=./.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=~/.vagrant.d/insecure_private_key -u vagrant ./vlad/playbooks/site.yml -t "$ANSIBLE_TAGS"
+fi
