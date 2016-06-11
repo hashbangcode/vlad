c7f9c911e606dc134ccce6ef9254f8596a4d74cf
Philip Norton <philipnorton42@gmail.com>
2016-06-11 23:20:32 +0100

    added the ability to run the vlad provisioning steps on the guest machine

---
+++ vlad_guts/scripts/vlad-play-internal.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Find the current directory of the script being run.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  TARGET="$(readlink "$SOURCE")"
+  if [[ $SOURCE == /* ]]; then
+    SOURCE="$TARGET"
+  else
+    DIR="$( dirname "$SOURCE" )"
+    SOURCE="$DIR/$TARGET"
+  fi
+done
+
+RDIR="$( dirname "$SOURCE" )"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# Change to the script directory.
+cd $DIR
+
+# Now move two directories above, which puts us into the Vagrant directory.
+cd ../../
+
+# Grab any tags that have been sent to the script.
+ANSIBLE_TAGS=$1
+
+if [ -z "$ANSIBLE_TAGS" ]; then
+    echo "Running Vlad playbook"
+    ansible-playbook -i "localhost," -c local --extra-vars "vlad_running_local=true" ./vlad_guts/playbooks/site.yml
+fi
+
+if [ ! -z "$ANSIBLE_TAGS" ]; then
+    echo "Running Vlad playbook with the tags '$ANSIBLE_TAGS'"
+    ansible-playbook -i "localhost," -c local --extra-vars "vlad_running_local=true" ./vlad_guts/playbooks/site.yml -t "$ANSIBLE_TAGS"
+fi
